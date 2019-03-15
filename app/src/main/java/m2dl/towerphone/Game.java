85593ec063cf9dc6aac5712a64329a756a7c3e4f
hadeshellife <hoang.minhhieu7693@gmail.com>
2019-03-15 11:44:59 +0100

    Merge branch 'interfaces' of https://github.com/laetitiatureau/TowerPhone
    # Conflicts:
    #	app/src/main/java/m2dl/towerphone/CanvasView.java
    #	app/src/main/java/m2dl/towerphone/Game.java

--- app/src/main/java/m2dl/towerphone/Game.java
+++ app/src/main/java/m2dl/towerphone/Game.java
@@ -1,12 +1,19 @@
 package m2dl.towerphone;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 
 public class Game extends AppCompatActivity {
 
+    private ProgressBar hpBar;
     private CanvasView customCanvas;
+    private int curHp = 0;
+
+    private Thread thread;
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +21,22 @@ public class Game extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         customCanvas = (CanvasView) findViewById(R.id.signature_canvas);
+        hpBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (curHp < 100)
+                    curHp++;
+                    android.os.SystemClock.sleep(50);
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            hpBar.setProgress(curHp);
+                        }
+                    });
+            }
+        });
     }
 
     public void clearCanvas(View v) {
