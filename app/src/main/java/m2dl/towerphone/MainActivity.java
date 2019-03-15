7a97ed36ec138730d6c0ce05a4b1f0f09e99220c
SarraBoussioud <saraboussioud@hotmail.fr>
2019-03-15 09:28:58 +0100

    Ajout des différentes interfaces

--- app/src/main/java/m2dl/towerphone/MainActivity.java
+++ app/src/main/java/m2dl/towerphone/MainActivity.java
@@ -1,7 +1,9 @@
 package m2dl.towerphone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void startGame(View view){
+        Intent intent = new Intent(this, Game.class);
+        startActivity(intent);
+    }
+
+    public void highScore(View view){
+        Intent intent = new Intent(this, HighScore.class);
+        startActivity(intent);
+    }
+
+    public void exit(View view){
+
+    }
+
+
 }
